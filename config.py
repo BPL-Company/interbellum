b3349f09e31b81c7a89541d3775a1e8a422f82b2
vezono <vezonolab@gmail.com>
2020-01-30 19:09:05 +0200

    Базовая оболочка

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '865293832:AAHD-ox6hi6Ws_pxBFb8VIp1uymHoMab2MM'
+creator = 792414733
---
+++ interbellum.py
@@ -0,0 +1,4 @@
+import config
+from modules.botutil import BotUtil
+
+bot = BotUtil(config.token, config.creator)
